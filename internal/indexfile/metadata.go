be51a44c76b0bdae940f4d16ea5699037adddb53
Exclearf <nboholii@gmail.com>
2026-09-05 11:44:07 +0200

    feat(indexfile): decode index metadata

--- internal/indexfile/metadata.go
+++ internal/indexfile/metadata.go
@@ -13,6 +13,18 @@ const (
 	fileMetadataRecordBytes = 12
 )
 
+const (
+	metadataFileBytes                      = fileHeaderBytes + metadataBodyBytes + fileFooterBytes
+	minimumFileBytes                       = fileHeaderBytes + fileFooterBytes
+	minimumDocumentOffsetsFileBytes        = minimumFileBytes + documentOffsetBytes
+	maximumFileBytes                uint64 = 1<<63 - 1
+)
+
+type indexMetadata struct {
+	Terms     TermFilesMetadata
+	Documents DocumentFilesMetadata
+}
+
 func WriteMetadataFile(
 	output io.Writer,
 	terms TermFilesMetadata,
@@ -47,6 +59,67 @@ func WriteMetadataFile(
 		return fmt.Errorf("finish metadata: %w", err)
 	}
 	return nil
+}
+
+func readMetadataFile(input io.Reader, size int64) (indexMetadata, error) {
+	if size != metadataFileBytes {
+		return indexMetadata{}, fmt.Errorf("metadata file size is %d, want %d", size, metadataFileBytes)
+	}
+
+	reader, err := newFileReader(input, size, metadataRole)
+	if err != nil {
+		return indexMetadata{}, err
+	}
+	var body [metadataBodyBytes]byte
+	if _, err := io.ReadFull(reader, body[:]); err != nil {
+		return indexMetadata{}, fmt.Errorf("read metadata body: %w", err)
+	}
+	if err := reader.finish(); err != nil {
+		return indexMetadata{}, fmt.Errorf("finish metadata: %w", err)
+	}
+
+	if analyzerID := binary.LittleEndian.Uint32(body[0:4]); analyzerID != analyzerContractID {
+		return indexMetadata{}, fmt.Errorf("unsupported analyzer ID %d", analyzerID)
+	}
+	if codecID := binary.LittleEndian.Uint32(body[4:8]); codecID != rawPostingsCodecID {
+		return indexMetadata{}, fmt.Errorf("unsupported postings codec ID %d", codecID)
+	}
+
+	metadata := indexMetadata{
+		Terms: TermFilesMetadata{
+			Terms:    decodeFileMetadata(body[8:20]),
+			Postings: decodeFileMetadata(body[20:32]),
+		},
+		Documents: DocumentFilesMetadata{
+			Lengths: decodeFileMetadata(body[32:44]),
+			Offsets: decodeFileMetadata(body[44:56]),
+			Data:    decodeFileMetadata(body[56:68]),
+		},
+	}
+	files := [...]struct {
+		name     string
+		metadata FileMetadata
+		minimum  uint64
+	}{
+		{"terms.bin", metadata.Terms.Terms, minimumFileBytes},
+		{"postings.bin", metadata.Terms.Postings, minimumFileBytes},
+		{"doclens.bin", metadata.Documents.Lengths, minimumFileBytes},
+		{"docids.off", metadata.Documents.Offsets, minimumDocumentOffsetsFileBytes},
+		{"docids.dat", metadata.Documents.Data, minimumFileBytes},
+	}
+	for _, file := range files {
+		if file.metadata.Length < file.minimum || file.metadata.Length > maximumFileBytes {
+			return indexMetadata{}, fmt.Errorf("invalid %s length %d", file.name, file.metadata.Length)
+		}
+	}
+	return metadata, nil
+}
+
+func decodeFileMetadata(data []byte) FileMetadata {
+	return FileMetadata{
+		Length:   binary.LittleEndian.Uint64(data[0:8]),
+		Checksum: binary.LittleEndian.Uint32(data[8:12]),
+	}
 }
 
 const rawPostingsCodecID = 1
--- internal/indexfile/metadata_test.go
+++ internal/indexfile/metadata_test.go
@@ -2,34 +2,98 @@ package indexfile
 
 import (
 	"bytes"
+	"encoding/binary"
+	"hash/crc32"
 	"testing"
 )
 
-func TestWriteMetadataFile(t *testing.T) {
-	terms := TermFilesMetadata{
+var (
+	goldenTermMetadata = TermFilesMetadata{
 		Terms:    FileMetadata{Length: 60, Checksum: 0xfd50af02},
 		Postings: FileMetadata{Length: 52, Checksum: 0x3d5463ec},
 	}
-	documents := DocumentFilesMetadata{
+	goldenDocumentMetadata = DocumentFilesMetadata{
 		Lengths: FileMetadata{Length: 20, Checksum: 0x00e08ad4},
 		Offsets: FileMetadata{Length: 36, Checksum: 0xfeed8a1b},
 		Data:    FileMetadata{Length: 14, Checksum: 0x20226602},
 	}
+)
+
+func TestReadMetadataFile(t *testing.T) {
+	metadata, err := readMetadataFile(bytes.NewReader([]byte(goldenMetadataFile)), int64(len(goldenMetadataFile)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := indexMetadata{
+		Terms:     goldenTermMetadata,
+		Documents: goldenDocumentMetadata,
+	}
+	if metadata != want {
+		t.Fatalf("metadata = %+v, want %+v", metadata, want)
+	}
+}
+
+func TestReadMetadataFileRejectsInvalidData(t *testing.T) {
+	corruptChecksum := []byte(goldenMetadataFile)
+	corruptChecksum[len(corruptChecksum)-1] ^= 1
+	tests := []struct {
+		name string
+		data []byte
+	}{
+		{"short file", []byte(goldenMetadataFile[:len(goldenMetadataFile)-1])},
+		{"long file", append([]byte(goldenMetadataFile), 0)},
+		{"wrong checksum", corruptChecksum},
+		{"unsupported analyzer", mutateMetadata(func(data []byte) {
+			binary.LittleEndian.PutUint32(data[8:12], analyzerContractID+1)
+		})},
+		{"unsupported codec", mutateMetadata(func(data []byte) {
+			binary.LittleEndian.PutUint32(data[12:16], rawPostingsCodecID+1)
+		})},
+		{"terms file below minimum", mutateMetadata(func(data []byte) {
+			binary.LittleEndian.PutUint64(data[16:24], fileHeaderBytes+fileFooterBytes-1)
+		})},
+		{"offset file below minimum", mutateMetadata(func(data []byte) {
+			binary.LittleEndian.PutUint64(data[52:60], fileHeaderBytes+fileFooterBytes)
+		})},
+		{"file above maximum", mutateMetadata(func(data []byte) {
+			binary.LittleEndian.PutUint64(data[64:72], maximumFileBytes+1)
+		})},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if _, err := readMetadataFile(bytes.NewReader(test.data), int64(len(test.data))); err == nil {
+				t.Fatal("readMetadataFile() error = nil")
+			}
+		})
+	}
+}
+
+func mutateMetadata(mutate func([]byte)) []byte {
+	data := []byte(goldenMetadataFile)
+	mutate(data)
+	checksum := crc32.Checksum(data[:len(data)-fileFooterBytes], crc32cTable)
+	binary.LittleEndian.PutUint32(data[len(data)-fileFooterBytes:], checksum)
+	return data
+}
+
+const goldenMetadataFile = "DSKMETA\x01" +
+	"\x01\x00\x00\x00\x01\x00\x00\x00" +
+	"\x3c\x00\x00\x00\x00\x00\x00\x00\x02\xaf\x50\xfd" +
+	"\x34\x00\x00\x00\x00\x00\x00\x00\xec\x63\x54\x3d" +
+	"\x14\x00\x00\x00\x00\x00\x00\x00\xd4\x8a\xe0\x00" +
+	"\x24\x00\x00\x00\x00\x00\x00\x00\x1b\x8a\xed\xfe" +
+	"\x0e\x00\x00\x00\x00\x00\x00\x00\x02\x66\x22\x20" +
+	"\xca\x54\x76\xe2"
+
+func TestWriteMetadataFile(t *testing.T) {
 
 	var output bytes.Buffer
-	if err := WriteMetadataFile(&output, terms, documents); err != nil {
+	if err := WriteMetadataFile(&output, goldenTermMetadata, goldenDocumentMetadata); err != nil {
 		t.Fatal(err)
 	}
 
-	want := "DSKMETA\x01" +
-		"\x01\x00\x00\x00\x01\x00\x00\x00" +
-		"\x3c\x00\x00\x00\x00\x00\x00\x00\x02\xaf\x50\xfd" +
-		"\x34\x00\x00\x00\x00\x00\x00\x00\xec\x63\x54\x3d" +
-		"\x14\x00\x00\x00\x00\x00\x00\x00\xd4\x8a\xe0\x00" +
-		"\x24\x00\x00\x00\x00\x00\x00\x00\x1b\x8a\xed\xfe" +
-		"\x0e\x00\x00\x00\x00\x00\x00\x00\x02\x66\x22\x20" +
-		"\xca\x54\x76\xe2"
-	if !bytes.Equal(output.Bytes(), []byte(want)) {
-		t.Fatalf("metadata file = % x, want % x", output.Bytes(), want)
+	if !bytes.Equal(output.Bytes(), []byte(goldenMetadataFile)) {
+		t.Fatalf("metadata file = % x, want % x", output.Bytes(), goldenMetadataFile)
 	}
 }
